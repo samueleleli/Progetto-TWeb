9b99801df09d6fe1d762193178e687fe6c5293ab
samueleleli <s1084424@studenti.univpm.it>
2020-05-26 12:47:38 +0200

    Modifica Navbar (home)

--- laraProject/resources/views/layouts/_navpublic.blade.php
+++ laraProject/resources/views/layouts/_navpublic.blade.php
@@ -4,8 +4,8 @@
                 <div class="row">
                     <div class="navbar-collapse collapse">
                         <ul class="nav navbar-nav">
-                            <li class="active"><a href="index.html">Home</a></li>
-                            <li><a href="shop.html">Catalogo Prodotti</a></li>
+                            <li class="active"><a href="{{ route('home') }}">Home</a></li>
+                            <li><a href="{{ route('catalog1') }}">Catalogo Prodotti</a></li>
                         </ul>
                     </div>  
                 </div>
