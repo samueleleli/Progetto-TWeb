73a2e8a9a5ebba8d44482ec29b6f738ab9f62527
Lisa <lisaburini.98@gmail.com>
2020-05-27 18:24:01 +0200

    change database.php

--- laraProject/config/database.php
+++ laraProject/config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+require( __DIR__ . '/../../include/connect.php');
+
 return [
 
     /*
@@ -46,11 +48,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $HOST,
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $DB,
+            'username' => $USER,
+            'password' => $PASSWORD,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
