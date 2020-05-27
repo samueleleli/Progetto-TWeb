0d789383bb464cb2cee9818e35f74a44462054fc
Michelangelo <michelangeloam98@gmail.com>
2020-05-27 18:12:47 +0200

    Update File register.blade.php
    Completato tutto il file.

--- laraProject/resources/views/auth/register.blade.php
+++ laraProject/resources/views/auth/register.blade.php
@@ -1,77 +1,98 @@
-@extends('layouts.app')
+@extends('layouts.public')
+
+@section('title', 'Registrazione')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+<div class="product-big-title-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="product-bit-title text-center">
+                    <h2>Registrazione</h2>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<div class="container area-main-content">
+    <div class="center">
+        <div class="row">
+            
+            {{ Form::open(array('route' => 'register', 'class' => 'contact-form')) }}
+       
+            <div class="center">
+                <div id="customer_details">
+                    <div class="col-5 form-login-signup">
+                        <h3>REGISTRAZIONE</h3>
+                        <p>
+                            {{ Form::label('name', 'Nome', ['class' => 'label-input']) }}
+                            {{ Form::text('name', '', ['class' => 'input', 'id' => 'name']) }}
+                            @if ($errors->first('name'))
+                            
+                            <ul class="errors">
+                                @foreach ($errors->get('name') as $message)
+                                <li>{{ $message }}</li>
+                                @endforeach
+                            </ul>
+                            @endif
+                        </p>
+                            
+                
+                        <p>
+                            {{ Form::label('surname', 'Cognome', ['class' => 'label-input']) }}
+                            {{ Form::text('surname', '', ['class' => 'input', 'id' => 'surname']) }}
+                            @if ($errors->first('surname'))
+                            
+                            <ul class="errors">
+                                @foreach ($errors->get('surname') as $message)
+                                <li>{{ $message }}</li>
+                                @endforeach
+                            </ul>
+                            @endif
+                        </p>
+
+                
+                        <p>
+                            {{ Form::label('username', 'Nome Utente', ['class' => 'label-input']) }}
+                            {{ Form::text('username', '', ['class' => 'input','id' => 'username']) }}
+                            @if ($errors->first('username'))
+                            <ul class="errors">
+                                @foreach ($errors->get('username') as $message)
+                                <li>{{ $message }}</li>
+                                @endforeach
+                            </ul>
+                            @endif
+                        </p>
+                            
+                        <p>
+                            {{ Form::label('password', 'Password', ['class' => 'label-input']) }}
+                            {{ Form::password('password', ['class' => 'input', 'id' => 'password']) }}
+                            @if ($errors->first('password'))
+                            <ul class="errors">
+                                @foreach ($errors->get('password') as $message)
+                                <li>{{ $message }}</li>
+                                @endforeach
+                            </ul>
+                            @endif
+                        </p>
+                            
+                        <p>
+                            {{ Form::label('password-confirm', 'Conferma password', ['class' => 'label-input']) }}
+                            {{ Form::password('password_confirmation', ['class' => 'input', 'id' => 'password-confirm']) }}
+                        </p>    
+    
+                        <p>
+                            {{ Form::submit('Registra', ['class' => 'form-btn1']) }}
+                        </p>
+                        
+                    </div>
+                </div>
+                {{ Form::close() }}
+            </div>
+        
+        </div>    
+    </div>                    
+</div>
+
 @endsection
