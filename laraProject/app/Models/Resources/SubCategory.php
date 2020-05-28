3d73cf618abecd0e4e89b532a8647fb0f8f43ca4
Simone <S1081623@studenti.univpm.it>
2020-05-28 13:30:44 +0200

    Update SubCategory.php

--- laraProject/app/Models/Resources/SubCategory.php
+++ laraProject/app/Models/Resources/SubCategory.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class SubCategory extends Model {
     
     protected $table = 'sottocategoria';
-    protected $primaryKey = 'catId';
-    public $timestamps = false;
-
+    protected $primaryKey = 'idSottocategoria';
+    public $timestamps = false; 
+    
     // Relazione One-To-One con Category
     /* Non sono ancora sicuro se ci servirà questa relazione uno a uno ma per sicurezza l'ho implementata
      * public function subCatToCat() {
