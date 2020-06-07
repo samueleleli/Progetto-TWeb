da743d71bf6c730598089ecdc3fbf72c4b802d92
samueleleli <s1084424@studenti.univpm.it>
2020-06-08 00:42:13 +0200

    Risolto il famoso Bug

--- laraProject/app/Http/Controllers/PublicController.php
+++ laraProject/app/Http/Controllers/PublicController.php
@@ -2,18 +2,19 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use App\Models\Catalog;
 
 class PublicController extends Controller {
     
     protected $_catalogModel;
-
+    use AuthenticatesUsers;
     public function __construct() {
         $this->_catalogModel = new Catalog;
     }
 
     public function showCatalog(Request $request) {
+    
         $route = request()->route()->getName();
         
         if($route=='catalog1') 
@@ -26,12 +27,29 @@ class PublicController extends Controller {
         //Prodotti di tutte le categorie
         $prods = $this->_catalogModel->getProdsByCat($cats->map->only(['idCategoria']));
         
-        if(!is_null($request->get('search'))){
-            $prods = $this->_catalogModel->getProds($request->get('search'));
+        $search = $request->get('search');
+        if(!is_null($search)){
+          if(!is_null(auth()->user()))
+          { 
+            //se è un utente registrato  
+            $role = auth()->user()->tipologiaUtente; //ottengo il livello
+            if($role == "user")
+            {
+              //se l'utente è di livello 1  
+              $prods = $this->_catalogModel->getProds($request->get('search')); //filtro dati
+            }
+          }
+          else
+          {
+            //nel caso non è un utente registrato              
+            $prods = $this->_catalogModel->getProds($request->get('search')); //filtro dati
+          }
         }
         
+        
+        
         return view('catalog')
-                        ->with('searchvalue',$request->get('search'))
+                        ->with('searchvalue',$search)
                         ->with('categories', $cats)
                         ->with('products', $prods) 
                         ->with('flagpub',$flagPub)
@@ -57,12 +75,28 @@ class PublicController extends Controller {
         //Prodotti della categoria selezionata 
         $prods = $this->_catalogModel->getProdsByCat([$catId], 3);
 
-        if(!is_null($request->get('search'))){
-            $prods = $this->_catalogModel->getProdsByCatSearch([$catId],$request->get('search'));
+        $search = $request->get('search');
+        if(!is_null($search)){
+          if(!is_null(auth()->user()))
+          { 
+            //se è un utente registrato  
+            $role = auth()->user()->tipologiaUtente; //ottengo il livello
+            if($role == "user")
+            {
+              //se l'utente è di livello 1  
+              $prods = $this->_catalogModel->getProdsByCatSearch([$catId],$request->get('search')); //filtro dati
+            }
+          }
+          else
+          {
+            //nel caso non è un utente registrato              
+            $prods = $this->_catalogModel->getProdsByCatSearch([$catId],$request->get('search')); //filtro dati
+          }
         }
+         
         return view('catalog')
                         ->with('route',$route)
-                        ->with('searchvalue',$request->get('search'))
+                        ->with('searchvalue',$search)
                         ->with('categories', $cats)
                         ->with('selectedCat', $selCat)
                         ->with('subCategories', $subCats)
@@ -90,13 +124,28 @@ class PublicController extends Controller {
         //Prodotti della sottocategoria selezionata
         $prods = $this->_catalogModel->getProdsBySubCat([$sotCatId]);
         
-        if(!is_null($request->get('search'))){
-            $prods = $this->_catalogModel->getProdsBySubCatSearch($sotCatId,$catId,$request->get('search'),3);
+        $search = $request->get('search');
+        if(!is_null($search)){
+          if(!is_null(auth()->user()))
+          { 
+            //se è un utente registrato  
+            $role = auth()->user()->tipologiaUtente; //ottengo il livello
+            if($role == "user")
+            {
+              //se l'utente è di livello 1  
+              $prods = $this->_catalogModel->getProdsBySubCatSearch($sotCatId,$catId,$request->get('search'),3); //filtro dati
+            }
+          }
+          else
+          {
+            //nel caso non è un utente registrato              
+            $prods = $this->_catalogModel->getProdsBySubCatSearch($sotCatId,$catId,$request->get('search'),3); //filtro dati
+          }
         }
         
         return view('catalog')
                         ->with('route',$route)
-                        ->with('searchvalue',$request->get('search'))
+                        ->with('searchvalue',$search)
                         ->with('categories', $cats)
                         ->with('selectedCat', $selCat)
                         ->with('subCategories', $subCats)
--- laraProject/resources/views/catalog.blade.php
+++ laraProject/resources/views/catalog.blade.php
@@ -6,151 +6,150 @@
 
 <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
 <script type="text/javascript">
-$(document).ready(function () {
-    $("a#delete").on('click', function() {
-        var domanda = confirm("Sei sicuro di voler cancellare il prodotto?");
-        if (domanda === true) {
-            return true;  
-        }else{
-            alert('Operazione annullata');
-            return false;
-        }
+    $(document).ready(function () {
+        $("a#delete").on('click', function () {
+            var domanda = confirm("Sei sicuro di voler cancellare il prodotto?");
+            if (domanda === true) {
+                return true;
+            } else {
+                alert('Operazione annullata');
+                return false;
+            }
+        });
     });
-});
 </script>
 
 @endsection
 
 @section('content')
 
-    <div class="product-big-title-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="product-bit-title text-center">
-                         @if($flagpub)
-                            <h2>Catalogo Prodotti</h2>
-                         @else
-                           @can('isStaff')
-                            <h2>Modifica Prodotti</h2>
-                           @endcan
-                         @endif
-                    </div>
+<div class="product-big-title-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="product-bit-title text-center">
+                    @if($flagpub)
+                        <h2>Catalogo Prodotti</h2>
+                    @else
+                     @can('isStaff')
+                        <h2>Modifica Prodotti</h2>
+                     @endcan
+                    @endif
                 </div>
             </div>
         </div>
     </div>
+</div>
 
-    <div class="single-product-area">
-        <div class="container">
-            <div class="row">
-                
-                <!-- inizio sezione laterale -->
-                <div class="col-md-4">
-                    @canany(['isStaff','isAdmin'])
+<div class="single-product-area">
+    <div class="container">
+        <div class="row">
+
+            <!-- inizio sezione laterale -->
+            <div class="col-md-4">
+                @canany(['isStaff','isAdmin'])
                     @else
-                        <div class="search-container">
-                        @if($route=='catalog1')
+                <div class="search-container">
+                    @if($route=='catalog1')
                         {{ Form::open(array('route' => $route,'method' => 'get'))}}
-                        @elseif($route=='catalog2')
+                    @elseif($route=='catalog2')
                         {{ Form::open(array('route' => [$route, $cat],'method' => 'get'))}}
-                        @elseif($route=='catalog3')
+                    @elseif($route=='catalog3')
                         {{ Form::open(array('route' => [$route, [$cat, $subcat]],'method' => 'get'))}}
-                        @endif
-                            {{Form::text('search','',['id' => 'search','placeholder' => 'Search..'])}}
+                    @endif
+                        {{Form::text('search',$searchvalue,['id' => 'search','placeholder' => 'Search..'])}}
                         {{ Form::submit('Search', ['class' => 'button']) }} 
                         {{Form::close()}}
-                        </div>
-                    @endcanany
-                    <div class="single-sidebar">
-                        <h2 class="sidebar-title">Categorie Prodotti</h2>
-                            <ul>
-                                @if($flagpub) 
-                                        @foreach ($categories as $category)
-                                            <li><a href="{{ route('catalog2', [$category->idCategoria])}}">{{ $category->categoria }}</a>
-                                        @endforeach
-                                @else
-                                   @can('isStaff')
-                                        @foreach ($categories as $category)
-                                            <li><a href="{{ route('catalog2Staff', [$category->idCategoria])}}">{{ $category->categoria }}</a>
-                                        @endforeach
-                                   @endcan     
-                                @endif
-                            </ul>
-                    </div>
-                    @isset($selectedCat)
-                        <div class="single-sidebar">
-                            <h2 class="sidebar-title">Sottocategorie in {{ $selectedCat->categoria }}</h2>
-                                <ul>
-                                    @if($flagpub) 
-                                        @foreach ($subCategories as $subCategory)
-                                            <li><a href="{{ route('catalog3', [$selectedCat->idCategoria, $subCategory->idSottocategoria])}}">{{ $subCategory->sottocategoria }}</a>
-                                        @endforeach
-                                    @else
-                                      @can('isStaff')
-                                        @foreach ($subCategories as $subCategory)
-                                            <li><a href="{{ route('catalog3Staff', [$selectedCat->idCategoria, $subCategory->idSottocategoria])}}">{{ $subCategory->sottocategoria }}</a>
-                                        @endforeach
-                                      @endcan
-                                    @endif    
-                                </ul>
-                        </div>
-                    @endisset()
                 </div>
-                
-                <!-- inizio sezione prodotti -->
-                <div class="col-md-8">
-                    <div class="row">
-                        @isset($products)
-                            @foreach ($products as $product)
-                               <div class="row rounded-corner">
-                                <div class="col-md-3 col-sm-3">
-                                    <div>
-                                        @include('helpers/productImg', ['imgFile' => $product->immagine])
-                                    </div>    
-                                </div>  
-                                <div class="col-md-9">
-                                    <div class="row">
-                                        <div class="col-md-12 col-sm-6">    
-                                           <div class="row"> 
-                                               <div class="col-sm-6">
-                                                    <h2>{{ $product->nome }}</h2>
-                                               </div>
-                                               <div class="col-sm-6">
-                                                @if(!$flagpub)
-                                              @can('isStaff')
-                                               <a href="{{ route('delproduct',[$product->idProdotto])}}" id="delete"> 
-                                                   <img src="{{ asset('images/trash.png') }} " width="45" height="45" alt="none" align="right"> 
+                @endcanany
+                <div class="single-sidebar">
+                    <h2 class="sidebar-title">Categorie Prodotti</h2>
+                    <ul>
+                        @if($flagpub) 
+                            @foreach ($categories as $category)
+                        <li><a href="{{ route('catalog2', [$category->idCategoria])}}">{{ $category->categoria }}</a>
+                            @endforeach
+                        @else
+                          @can('isStaff')
+                            @foreach ($categories as $category)
+                        <li><a href="{{ route('catalog2Staff', [$category->idCategoria])}}">{{ $category->categoria }}</a>
+                            @endforeach
+                           @endcan     
+                        @endif
+                    </ul>
+                </div>
+                @isset($selectedCat)
+                <div class="single-sidebar">
+                    <h2 class="sidebar-title">Sottocategorie in {{ $selectedCat->categoria }}</h2>
+                    <ul>
+                        @if($flagpub) 
+                            @foreach ($subCategories as $subCategory)
+                                <li><a href="{{ route('catalog3', [$selectedCat->idCategoria, $subCategory->idSottocategoria])}}">{{ $subCategory->sottocategoria }}</a>
+                            @endforeach
+                        @else
+                          @can('isStaff')
+                            @foreach ($subCategories as $subCategory)
+                                <li><a href="{{ route('catalog3Staff', [$selectedCat->idCategoria, $subCategory->idSottocategoria])}}">{{ $subCategory->sottocategoria }}</a>
+                            @endforeach
+                          @endcan
+                        @endif    
+                    </ul>
+                </div>
+                @endisset()
+            </div>
+
+            <!-- inizio sezione prodotti -->
+            <div class="col-md-8">
+                <div class="row">
+                    @isset($products)
+                    @foreach ($products as $product)
+                    <div class="row rounded-corner">
+                        <div class="col-md-3 col-sm-3">
+                            <div>
+                                @include('helpers/productImg', ['imgFile' => $product->immagine])
+                            </div>    
+                        </div>  
+                        <div class="col-md-9">
+                            <div class="row">
+                                <div class="col-md-12 col-sm-6">    
+                                    <div class="row"> 
+                                        <div class="col-sm-6">
+                                            <h2>{{ $product->nome }}</h2>
+                                        </div>
+                                        <div class="col-sm-6">
+                                            @if(!$flagpub)
+                                                @can('isStaff')
+                                                <a href="{{ route('delproduct',[$product->idProdotto])}}" id="delete"> 
+                                                    <img src="{{ asset('images/trash.png') }} " width="45" height="45" alt="none" align="right"> 
                                                 </a>
                                                 <a href="{{ route('modproduct',[$product->idProdotto])}}"> 
                                                     <img src="{{ asset('images/modify.jpg') }} " width="30" height="30" alt="none" align="right"> 
                                                 </a>
-                                               
-                                              @endcan
+                                                @endcan
                                             @endif
-                                               </div>
-                                            
-                                            
-                                           </div>
-                                            <h4>Descrizione breve: </h4><p class="descr">{{ $product->descrBreve }}</p>                                        
-                                            <h4>Descrizione estesa: </h4><p class="descr">{{ $product->descrEstesa }}</p>
-                                            
-                                            @include('helpers/productPrice')
                                         </div>
+
+
                                     </div>
+                                    <h4>Descrizione breve: </h4><p class="descr">{{ $product->descrBreve }}</p>                                        
+                                    <h4>Descrizione estesa: </h4><p class="descr">{{ $product->descrEstesa }}</p>
+
+                                    @include('helpers/productPrice')
                                 </div>
-                               </div>
-                            @endforeach
-                            
-                            <!--Paginazione-->
-                            @include('pagination.paginator', ['paginator' => $products, 'search' => $searchvalue])
-                            
-                        @endisset()
+                            </div>
+                        </div>
                     </div>
+                    @endforeach
+
+                    <!--Paginazione-->
+                    @include('pagination.paginator', ['paginator' => $products, 'search' => $searchvalue])
+
+                    @endisset()
                 </div>
-                
             </div>
+
         </div>
     </div>
+</div>
 
 @endsection
